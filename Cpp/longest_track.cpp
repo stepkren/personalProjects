e1860af1ee0659f2fed9e7d77c774406432b0101
Stepan Krenovsky <krenoste@fit.cvut.cz>
2024-02-18 23:12:05 +0100

    Added the longest_track problem solution

---
+++ Cpp/longest_track.cpp
@@ -0,0 +1,204 @@
+#ifndef __PROGTEST__
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cstdint>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <optional>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+enum Point : size_t {};
+
+struct Path {
+  Point from, to;
+  unsigned length;
+
+  Path(size_t f, size_t t, unsigned l) : from{f}, to{t}, length{l} {}
+
+  friend bool operator == (const Path& a, const Path& b) {
+    return std::tie(a.from, a.to, a.length) == std::tie(b.from, b.to, b.length);
+  }
+  
+  friend bool operator != (const Path& a, const Path& b) { return !(a == b); }
+};
+
+#endif
+
+std::vector<Point> top_sort ( const std::vector<std::vector<std::pair<Point, size_t>>> & adjList, std::map<Point, size_t> & inDegree ){
+  std::queue<Point> q;
+  std::vector<Point> res;
+
+  for ( const auto & vertex : inDegree ){
+    if ( vertex.second == 0 ){
+      q.push ( vertex.first );
+    }
+  }
+
+  while ( !q.empty() ){
+    auto curr = q.front();
+    q.pop();
+    res.push_back ( curr );
+    for ( const auto & neighbour : adjList[curr] ){
+      inDegree[neighbour.first]--;
+      if ( inDegree[neighbour.first] == 0 ){
+        q.push ( neighbour.first );
+      }
+    }
+  }
+
+  return res;
+}
+
+std::vector<Path> reconstruct_longest_path ( const std::map<Point, Point> & parent, const std::vector<Point> & sourceNodes, const int end, const std::vector<int> & distances ){
+  std::vector<Path> res;
+  size_t curr = end;
+
+
+  auto it = std::find (sourceNodes.begin(), sourceNodes.end(), Point {curr});
+  while ( it == sourceNodes.end() ){
+    Path tmp_path ( (parent.find( Point {curr} )) -> second, curr, unsigned(- (distances[curr] - distances[(parent.find( Point {curr} )) -> second])) );
+    res.push_back( tmp_path );
+    curr = (parent.find( Point {curr} )) -> second;
+    it = std::find (sourceNodes.begin(), sourceNodes.end(), Point {curr});
+  }
+
+  std::reverse ( res.begin(), res.end() );
+
+  return res;
+}
+
+std::vector<Path> Bellman_Ford ( const std::vector<std::vector<std::pair<Point, size_t>>> & adjList, const std::vector<Point> tOrder, const std::vector<Point> srcNodes ){
+  std::vector<Path> res;
+  std::map<Point, Point> parent;
+  std::vector<int> dist(adjList.size(), std::numeric_limits<int>::max());
+
+  for ( const auto & srcNode : srcNodes ){
+    dist[srcNode] = 0;
+  }
+  
+  for ( const auto & vertex : tOrder ){
+    for ( const auto & neigh : adjList[vertex] ){
+      if ( dist[vertex] - (int)neigh.second < dist[neigh.first] ){
+        dist[neigh.first] = dist[vertex] - (int)neigh.second;
+        parent[neigh.first] = vertex;
+      }
+    }
+  }
+
+  auto minDistance = std::min_element(dist.begin(), dist.end());
+  int minIndex = std::distance(dist.begin(), minDistance);
+
+  std::vector<Path> result = reconstruct_longest_path ( parent, srcNodes, minIndex, dist );
+
+  return result;
+}
+
+std::vector<Path> longest_track(size_t points, const std::vector<Path>& all_paths){
+  std::vector<std::vector<std::pair<Point, size_t>>> adjList(points);
+  std::set<Point> verteces;
+  std::vector<Point> sourceNodes;
+  size_t neighListSize = 0;
+
+  for ( const auto & Edge : all_paths ){
+    adjList[Edge.from].push_back( std::make_pair( Edge.to, Edge.length ) );
+    verteces.emplace( Edge.from );
+    verteces.emplace( Edge.to );
+  }
+
+  std::map<Point, size_t> vertexDegIn;
+
+  for ( const auto & vertex : verteces ){
+    vertexDegIn[vertex] = 0;
+  }
+
+  for ( const auto & vertex : verteces ){
+    neighListSize = adjList[vertex].size();
+    for ( size_t j = 0; j < neighListSize; j++ ){
+      vertexDegIn[adjList[vertex][j].first] += 1;
+    }
+  }
+
+  for ( const auto & vertex : vertexDegIn ){
+    if ( vertex.second == 0 ){
+      sourceNodes.push_back( vertex.first );
+    }
+  }
+
+  std::vector<Point> topologicalOrderOfGraph = top_sort ( adjList, vertexDegIn );
+
+  std::vector<Path> result = Bellman_Ford ( adjList, topologicalOrderOfGraph, sourceNodes );
+
+  return result;
+}
+
+
+#ifndef __PROGTEST__
+
+
+struct Test {
+  unsigned longest_track;
+  size_t points;
+  std::vector<Path> all_paths;
+};
+
+inline const Test TESTS[] = {
+  {13, 5, { {3,2,10}, {3,0,9}, {0,2,3}, {2,4,1} } },
+  {11, 5, { {3,2,10}, {3,1,4}, {1,2,3}, {2,4,1} } },
+  {16, 8, { {3,2,10}, {3,1,1}, {1,2,3}, {1,4,15} } },
+};
+
+#define CHECK(cond, ...) do { \
+    if (cond) break; \
+    printf("Fail: " __VA_ARGS__); \
+    printf("\n"); \
+    return false; \
+  } while (0)
+
+bool run_test(const Test& t) {
+  auto sol = longest_track(t.points, t.all_paths);
+
+  unsigned length = 0;
+  for (auto [ _, __, l ] : sol) length += l;
+
+  CHECK(t.longest_track == length,
+    "Wrong length: got %u but expected %u", length, t.longest_track);
+
+  for (size_t i = 0; i < sol.size(); i++) {
+    CHECK(std::count(t.all_paths.begin(), t.all_paths.end(), sol[i]),
+      "Solution contains non-existent path: %zu -> %zu (%u)",
+      sol[i].from, sol[i].to, sol[i].length);
+
+    if (i > 0) CHECK(sol[i].from == sol[i-1].to,
+      "Paths are not consecutive: %zu != %zu", sol[i-1].to, sol[i].from);
+  }
+
+  return true;
+}
+#undef CHECK
+
+int main() {
+  int ok = 0, fail = 0;
+
+  for (auto&& t : TESTS) (run_test(t) ? ok : fail)++;
+  
+  if (!fail) printf("Passed all %i tests!\n", ok);
+  else printf("Failed %u of %u tests.\n", fail, fail + ok);
+}
+
+#endif
+
+
